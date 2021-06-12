c27846fe6e1888721aca7cf3ac1c61a6cec39856
turbo <nirmal@flairlabs.com>
2021-06-12 18:38:13 +0530

    Updates

---
+++ precache-manifest.2dcc5c3a8d1df9b836dc0e5a06361009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91333e7afc2d910c547760ee31728643",
+    "url": "/new-navbar/index.html"
+  },
+  {
+    "revision": "4b4aed8176d5d47ff60f",
+    "url": "/new-navbar/static/css/main.d117c123.chunk.css"
+  },
+  {
+    "revision": "4a210ac64534cf247454",
+    "url": "/new-navbar/static/js/2.6ed37cc7.chunk.js"
+  },
+  {
+    "revision": "4b4aed8176d5d47ff60f",
+    "url": "/new-navbar/static/js/main.e7a2b2dc.chunk.js"
+  },
+  {
+    "revision": "83a004a90170e4172776",
+    "url": "/new-navbar/static/js/runtime-main.89861be1.js"
+  },
+  {
+    "revision": "1d08b615498cf92c5172d1b625f293a1",
+    "url": "/new-navbar/static/media/zaveri-kart.1d08b615.png"
+  }
+]);
